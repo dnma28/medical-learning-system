1d17cf7bcc235a0e5b8f75b7e35d373088cee042
dnma28 <horrorlide321@gmail.com>
2026-09-24 00:53:38 +0700

    hoc90: export source-aware runtime contracts

--- src/medical_learning_system/hoc90/__init__.py
+++ src/medical_learning_system/hoc90/__init__.py
@@ -1,3 +1,14 @@
+from .blueprint import (
+    BlueprintStatus,
+    Hoc90Blueprint,
+    MasteryTarget,
+)
+from .bootstrap import (
+    BootstrapMode,
+    BootstrapPlan,
+    Hoc90Command,
+    build_bootstrap_plan,
+)
 from .session import (
     Hoc90Session,
     LearningEvent,
@@ -5,13 +16,22 @@ from .session import (
     SessionCheckpoint,
     SessionStage,
     SessionStatus,
+    SourceSpineRef,
 )
 
 __all__ = [
+    "BlueprintStatus",
+    "Hoc90Blueprint",
+    "MasteryTarget",
+    "BootstrapMode",
+    "BootstrapPlan",
+    "Hoc90Command",
+    "build_bootstrap_plan",
     "Hoc90Session",
     "LearningEvent",
     "LearningEventType",
     "SessionCheckpoint",
     "SessionStage",
     "SessionStatus",
+    "SourceSpineRef",
 ]
